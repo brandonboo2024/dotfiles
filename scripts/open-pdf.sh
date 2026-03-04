62fffac8e2463ec80e887eb68c21589b0dd9fe5d
brandonboo2024 <brandon.boo.2024@computing.smu.edu.sg>
2026-03-04 20:17:01 +0800

    change pdf opening script to sioyek

--- scripts/open-pdf.sh
+++ scripts/open-pdf.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 
-file="$(fd --full-path -e pdf "$PWD" | fzf)" || exit
-[ -n "$file" ] && tmux run-shell -b "nohup zathura \"${file}\" >/dev/null 2>&1 </dev/null" 
+file="$(fd -e pdf . "$PWD" | fzf)" || exit 0
+# [ -n "$file" ] && tmux run-shell -b "nohup sioyek-x11 \"${file}\" >/dev/null 2>&1 </dev/null" 
+
+setsid -f env QT_QPA_PLATFORM=xcb sioyek "$file" >/tmp/sioyek.log 2>&1 </dev/null 
+
+# exec env QT_QPA_PLATFORM=xcb sioyek "$file"
 
