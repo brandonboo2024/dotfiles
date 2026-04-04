1f9bc9b399b219d6fe8d2ec549e071ddc33b9b8e
brandonboo2024 <brandon.boo.2024@computing.smu.edu.sg>
2026-04-04 11:10:02 +0800

    fix sioyek on desktop

--- scripts/open-pdf.sh
+++ scripts/open-pdf.sh
@@ -4,7 +4,8 @@
 file="$(fd -e pdf -e epub . "$PWD" | fzf)" || exit 0
 # [ -n "$file" ] && tmux run-shell -b "nohup sioyek-x11 \"${file}\" >/dev/null 2>&1 </dev/null" 
 
-setsid -f env QT_QPA_PLATFORM=xcb sioyek "$file" >/tmp/sioyek.log 2>&1 </dev/null 
+# setsid -f env QT_QPA_PLATFORM=xcb sioyek "$file" >/tmp/sioyek.log 2>&1 </dev/null 
+setsid -f env sioyek-x11 "$file" >/tmp/sioyek.log 2>&1 </dev/null 
 
 # exec env QT_QPA_PLATFORM=xcb sioyek "$file"
 
