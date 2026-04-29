5da0eecc7e61d56b8c84e3bb4f495d2b653bd3d0
brandonboo2024 <brandon.boo.2024@computing.smu.edu.sg>
2026-04-29 17:09:16 +0800

    keyring daemon

--- mango/autostart.sh
+++ mango/autostart.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+pkill -f gnome-keyring 2>/dev/null || true
+
+eval "$(gnome-keyring-daemon --start --components=secrets)"
+export SSH_AUTH_SOCK
+dbus-update-activation-environment --systemd SSH_AUTH_SOCK
+
 # desktop-session-start >/dev/null 2>&1 &
 dms run -d
 
@@ -8,6 +14,8 @@ wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
 
 # clipboard content manager
 wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
+
+
 
 # inhibit by audio
 # sway-audio-idle-inhibit >/dev/null 2>&1 &
