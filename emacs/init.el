7d3b1c99a13468ca2a8eb3876e41540e7b78d305
brandonboo2024 <brandonboojunwei@gmail.com>
2026-04-30 11:32:03 +0800

    emacs

---
+++ emacs/init.el
@@ -0,0 +1,6 @@
+(setq inhibit-startup-message t)
+
+(scroll-bar-mode - 1) ; disable scroll bar
+(tool-bar-mode -1) ; disable toolbar
+(tooltip-mode -1) ; disable tooltips
+(set-fringe-mode 10) ; give some breathing room
