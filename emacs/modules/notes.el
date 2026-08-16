101490096a3ffff525d875f35add9a661fb867dd
jwboo <jwboo@posteo.com>
2026-08-16 17:46:53 +0800

    add slack and remove comment

--- emacs/modules/notes.el
+++ emacs/modules/notes.el
@@ -1,8 +1,5 @@
 ;; -*- lexical-binding: t; -*-
 
-;; Keep Org deliberately bare while the organisation system is rebuilt from
-;; actual use.  Files, captures, agendas, task states and advanced note tooling
-;; can be added later without carrying assumptions from the previous setup.
 (use-package org
   :straight nil
   :custom
